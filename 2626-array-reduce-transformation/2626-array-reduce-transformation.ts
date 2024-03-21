ca2bc7b983d85b41ace1891bdcf384457a49af06
snehalkoli <67510836+snehalkoli@users.noreply.github.com>
2024-03-21 10:52:38 +0530

    Time: 49 ms (93.75%), Space: 51.3 MB (94.54%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.ts
@@ -0,0 +1,12 @@
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+if (nums.length != 0){
+var val = fn(init, nums[0]);
+for(let i = 1; i < nums.length; i++){
+val = fn(val, nums[i]);
+}
+return val;
+}
+return init;
+};
